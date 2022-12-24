f19e2ffa3b4bcf52391e6f52078715752c68cbd9
Randy Wressell <randy5235@gmail.com>
2022-12-23 16:39:10 -0800

    Third commit

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -5,20 +5,22 @@ import './Child.css';
 function Child(props) {
     const [count, setCount] = useState(0)
     const [lost, setLost] = useState(0)
+    const reset = () => {
+        setLost(0);
+        setCount(0);
+    }
     return (
         <div className="Child">
             <div className="Display">
            <h1>{props.name}</h1>
-                &#9733; Earned:
-                {count - lost || 0}
+                &#9733; Earned: {count - lost || 0}
             <br></br>
-                &#9733; Lost:
-                {lost || 0}
+                &#9733; Lost: {lost || 0}
             </div>
             <div className='Actions'>
-                <button onClick={() => setCount(count + 1)} >Add &#9733;</button>
-                <button onClick={() => setLost(lost + 1)} >Lose &#9733;</button>
-                <button onClick={() => { setLost(0); setCount(0); }} >Reset &#9733;</button>
+                <button onClick={() => setCount(count + 1)} > Add &#9733;</button>
+                <button onClick={() => setLost(lost + 1)} > Lose &#9733;</button>
+                <button onClick={() => reset()} > Reset &#9733;</button>
             </div>
         </div>
     );
